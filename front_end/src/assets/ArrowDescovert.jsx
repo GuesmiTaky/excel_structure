b210db0a28d38d0b1e2450ce0723862e1ed43334
GuesmiTaky <guesmi.taky@gmail.com>
2023-11-21 22:49:41 +0100

    Fix svg

--- front_end/src/assets/ArrowDescovert.jsx
+++ front_end/src/assets/ArrowDescovert.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const ArrowDescovert = () => {
-    return (
-        <svg width="18" height="17" viewBox="0 0 18 17" fill="white" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1.5 8.43542L15.2232 8.29857M10.1182 1.91138L16.6412 8.43436L9.98677 15.0887" stroke="white" strokeWidth="2" stroke-linecap="square" stroke-linejoin="round" />
-        </svg>
-    )
-}
+  return (
+    <svg
+      width="18"
+      height="17"
+      viewBox="0 0 18 17"
+      fill="white"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1.5 8.43542L15.2232 8.29857M10.1182 1.91138L16.6412 8.43436L9.98677 15.0887"
+        stroke="white"
+        strokeWidth="2"
+        strokeLinecap="square"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
 
-export default ArrowDescovert
+export default ArrowDescovert;
--- front_end/src/assets/Itinerary.jsx
+++ front_end/src/assets/Itinerary.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import React from "react";
 
 const Itinerary = () => {
-    return (
-        <svg width="38" height="38" viewBox="0 0 38 38" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M3.68848 12.3456V27.6891C3.68848 30.6853 5.81732 31.9153 8.40348 30.433L12.1092 28.3199C12.9135 27.8626 14.2539 27.8153 15.0896 28.241L23.3685 32.3883C24.2042 32.7983 25.5446 32.7668 26.3489 32.3095L33.1769 28.3987C34.0442 27.8941 34.7696 26.6641 34.7696 25.6549V10.3114C34.7696 7.31526 32.6408 6.08526 30.0546 7.56757L26.3489 9.68065C25.5446 10.138 24.2042 10.1853 23.3685 9.75949L15.0896 5.62795C14.2539 5.21795 12.9135 5.24949 12.1092 5.7068L5.28117 9.61757C4.39809 10.1222 3.68848 11.3522 3.68848 12.3456Z" stroke="#005645" strokeWidth="1.5" stroke-linecap="round" stroke-linejoin="round" />
-            <path d="M13.5757 6.38477V26.8848" stroke="#005645" strokeWidth="1.5" stroke-linecap="round" stroke-linejoin="round" />
-            <path d="M24.8823 10.5166V31.6158" stroke="#005645" strokeWidth="1.5" stroke-linecap="round" stroke-linejoin="round" />
-        </svg>
-    )
-}
+  return (
+    <svg
+      width="38"
+      height="38"
+      viewBox="0 0 38 38"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M3.68848 12.3456V27.6891C3.68848 30.6853 5.81732 31.9153 8.40348 30.433L12.1092 28.3199C12.9135 27.8626 14.2539 27.8153 15.0896 28.241L23.3685 32.3883C24.2042 32.7983 25.5446 32.7668 26.3489 32.3095L33.1769 28.3987C34.0442 27.8941 34.7696 26.6641 34.7696 25.6549V10.3114C34.7696 7.31526 32.6408 6.08526 30.0546 7.56757L26.3489 9.68065C25.5446 10.138 24.2042 10.1853 23.3685 9.75949L15.0896 5.62795C14.2539 5.21795 12.9135 5.24949 12.1092 5.7068L5.28117 9.61757C4.39809 10.1222 3.68848 11.3522 3.68848 12.3456Z"
+        stroke="#005645"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M13.5757 6.38477V26.8848"
+        stroke="#005645"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M24.8823 10.5166V31.6158"
+        stroke="#005645"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
 
-export default Itinerary
+export default Itinerary;
